f33ddbf723cf8f03777fd5b96cfc85eccf0e3976
Aleksei Vedernikov <alexei.vedder@gmail.com>
2021-04-04 15:20:20 +0400

    iterationsTotal was replaced with epsilon, added a function to resolve the iterations total

--- src/app/components/plot/plot.component.ts
+++ src/app/components/plot/plot.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {DataService} from '../../services/data.service';
-import {pi, sin, sqrt, max} from 'mathjs';
+import {ceil, max, pi, sin, sqrt} from 'mathjs';
 import {GivenData} from '../../models/given-data.model';
 
 @Component({
@@ -12,21 +12,41 @@ export class PlotComponent implements OnInit {
 
 	@Output()
 	public readonly initialized: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-	private data: GivenData;
-
 	public board;
 	public timeSlider = {Value: () => 0};
 	public plot;
 	public leftBorder;
 	public rightBorder;
+	private data: GivenData;
 
-	// TODO fix a bug: N1 and N2 is too huge: more than 1e34 when Epsilon is 0.0001
-	// I guess the root cause is in the error estimate
+
+
+	constructor(private dataService: DataService) {
+	}
+
+	public ngOnInit(): void {
+		this.data = this.dataService.model;
+		this.initializeGraph();
+	}
+
+	public returnToInit() {
+		this.initialized.emit(false);
+	}
+
+	// TODO fix a bug: N2 is too huge
 	private findIterationNum = () => {
-		let N1 = Math.ceil(((this.omega() ** 2 - this.omegaTop() ** 2) * this.data.L ** 2) / (this.data.epsilon * (pi ** 3) * this.data.c ** 2) - 1);
-		let N2 = Math.ceil(((2 * this.omega() * this.data.c * (this.data.L ** 2)) / this.data.epsilon) - 1);
-		return max(N1, N2);
+		let a1 = this.omega() ** 2 - this.omegaTop() ** 2,
+			a2 = (this.data.L ** 2),
+			b1 = this.data.epsilon * (pi ** 3) * (this.data.c ** 2);
+
+		const N1 = ceil(sqrt(a1 * a2 / b1) - 1);
+
+		let d1 = 2 * this.omega() * this.data.c * (this.data.L ** 2);
+
+		let N2 = ceil((d1 / this.data.epsilon) - 1);
+
+		// TODO uncomment once the bug is fixed
+		return max(N1/*, N2*/);
 	};
 
 	private Ey = (z: number, x: number, t: number, n: number) => (this.U(z, t, n) + this.Teta(z, t)) * sin(pi * x / this.data.l);
@@ -56,14 +76,6 @@ export class PlotComponent implements OnInit {
 
 	private findEy = (z: number) => this.Ey(z, this.data.l / 2, this.timeSlider.Value(), this.findIterationNum());
 
-	constructor(private dataService: DataService) {
-	}
-
-	public ngOnInit(): void {
-		this.data = this.dataService.model;
-		this.initializeGraph();
-	}
-
 	private initializeGraph() {
 		const left = -this.data.L / 8;
 		const right = 1.1 * this.data.L;
@@ -83,9 +95,5 @@ export class PlotComponent implements OnInit {
 		this.plot = this.board.create('functiongraph', [this.findEy, 0, this.data.L]);
 		this.leftBorder = this.board.create('line', [[0, 0], [0, 1]], {strokeColor: 'red'});
 		this.rightBorder = this.board.create('line', [[this.data.L, 0], [this.data.L, 1]], {strokeColor: 'red'});
-	}
-
-	public returnToInit() {
-		this.initialized.emit(false);
 	}
 }
